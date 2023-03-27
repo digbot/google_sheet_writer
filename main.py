3de131254b69dbacece2d9c6050d233d239a2b4e
Dimitar Sashev Dimitrov <dimitar.sashev@gmail.com>
2023-03-27 12:07:15 +0300

    Worksheet.clean removed

--- main.py
+++ main.py
@@ -1,45 +1,16 @@
-import pickle
-import os.path
-import os
-import base64
-from google.auth.transport.requests import Request
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from datetime import datetime, time, timedelta
-import google.auth
-from google.oauth2.credentials import Credentials
 import gspread
 from helpers.commonHelper import extract_bgn_numbers_and_dates
 from helpers.storeHelper import get_sheet_id, append_to_json_file, get_processed_ids
 from service.sheet.sheetService import get_first_empty_row, get_sheet
-from service.gmail.gmailService import get_gmail_service
+from service.gmail.gmailService import get_gmail_service, get_gmail_cred
 
 # Define the scopes that the application will need
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly',
           'https://www.googleapis.com/auth/drive',
           'https://www.googleapis.com/auth/spreadsheets']
 
-def get_gmail_cred():
-    """Gets the Gmail API service"""
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first time
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-    return creds
 
 def fetch_message(service, search_query):
     query = "subject:" + search_query
