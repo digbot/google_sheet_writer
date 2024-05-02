de222732994f58320dd237b7cc33f5a1dcd45914
Dimitar Sashev Dimitrov <dimitar.sashev@gmail.com>
2024-05-02 14:03:16 +0300

    add_details

--- service/httpClient.py
+++ service/httpClient.py
@@ -7,7 +7,8 @@ def send_post_request(url, data):
         print(data) 
         response = requests.post(url, json=data)
         response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
-        print("POST request sent successfully.")
+        print("POST request sent successfully: ")
+        print(response.text)
     except requests.exceptions.RequestException as e:
         print(f"Error sending POST request: {e}")
 
@@ -27,4 +28,4 @@ def send_month_data(inData, outData, buffer, invest):
     }
 
     # Send the POST request
-    send_post_request(url, json_data)
+    return send_post_request(url, json_data)
